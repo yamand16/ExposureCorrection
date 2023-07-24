980cf0d6e1a000f94ea64882a3e33c98aaa1c282
Dogucan Yaman <41586251+yamand16@users.noreply.github.com>
2023-07-24 13:35:31 +0200

    torchmetrics is added into evaluate_exposure.py

--- evaluate_exposure.py
+++ evaluate_exposure.py
@@ -9,6 +9,7 @@ from skimage.metrics import structural_similarity as ssim
 from sklearn.metrics import mean_squared_error as mse
 import argparse
 import os
+import torchmetrics
 
 parser = argparse.ArgumentParser(description='Evaluation')
 parser.add_argument("--images_path1", type=str, default="expert_a_testing_set", help="input path of the GT set")
@@ -27,14 +28,17 @@ GT_E = sorted(glob.glob("../dataset/exposure/test/expert_e_testing_set/*"))
 GT = sorted(glob.glob(os.path.join(args.images_path1, "*")))
 predicted_images = sorted(glob.glob(os.path.join(args.images_path2, "*")))
 
-def calculate_PSNR(MSE, MAX_pixel = 255.0):
+def calculate_PSNR(MSE, MAX_pixel = 255.0, torch_version=False):
 	"""
 		PSNR implementation
 		For the pytorch version, please visit https://pytorch.org/ignite/generated/ignite.metrics.PSNR.html
 		MSE: Calculated mean squared error
 		MAX_pixel: The maximum possible pixel value
 	"""
-    return 20 * math.log10(MAX_pixel / math.sqrt(MSE))
+	if torch_version:
+		return torchmetrics.functional.peak_signal_noise_ratio(image1, image2)
+	else:
+		return 20 * math.log10(MAX_pixel / math.sqrt(MSE))
 	#return 10 * math.log10((MAX_pixel * MAX_pixel) / MSE)
 
 def L1_distance(image1, image2):
@@ -44,12 +48,15 @@ def L2_distance(image1, image2):
 	#return np.linalg.norm(image1 - image2)
 	return (mse(image1[:,:,0], image2[:,:,0]) + mse(image1[:,:,1], image2[:,:,1]) + mse(image1[:,:,2], image2[:,:,2])) / 3
 
-def calculate_SSIM(image1, image2, data_range=2, multichannel=True):
+def calculate_SSIM(image1, image2, data_range=2, multichannel=True, torch_version=False):
 	"""
 		data_range: the absolute difference between the maximum possible pixel value and the minimum possible pixel value.
 		multichannel: whether the image has multichannel.
 	"""
-	return ssim(image1, image2, data_range=data_range, multichannel=True)
+	if torch_version:
+		return torchmetrics.StructuralSimilarityIndexMeasure(data_range=data_range)
+	else:
+		return ssim(image1, image2, data_range=data_range, multichannel=True)
 
 PSNR = SSIM = L1 = L2 = 0.0
 
